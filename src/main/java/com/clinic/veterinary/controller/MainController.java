8d768ab6435e1d5f10dfdee2cdc335119fc19168
suuuinkim <suchae7@naver.com>
2023-05-04 23:42:49 +0900

    [수정] 주석 설정

--- src/main/java/com/clinic/veterinary/controller/MainController.java
+++ src/main/java/com/clinic/veterinary/controller/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    /**
+     * 로그인 페이지
+     */
     @GetMapping("/")
     public String main(){
         return "login";
